6a03e6e8ee94a62dd9f85f9d3a20a1a4bacba998
taohuadao <taohuadao@taohuadaodeMac-mini.local>
2017-11-29 16:47:51 +0800

    xxx

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by taohuadao on 2017/11/29.
+//  Copyright © 2017年 taohuadao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by taohuadao on 2017/11/29.
+//  Copyright © 2017年 taohuadao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
